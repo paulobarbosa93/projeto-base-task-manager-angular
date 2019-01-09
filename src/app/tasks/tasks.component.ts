edf37e585e040d22bac065c5b191da38a90a21f2
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2019-01-08 23:03:32 -0300

    ngOnInit

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tasks',
+  templateUrl: './tasks.component.html'
+})
+
+export class TasksComponent implements OnInit {
+  public ngOnInit(){
+
+  }
+}
