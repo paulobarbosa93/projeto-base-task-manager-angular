c22b1c5173c2d70139d84612f28a82e5d186b2e8
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2019-01-10 22:36:43 -0300

    adding promise to TaskService

--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -15,7 +15,16 @@ const TASKS: Array<Task> = [
 @Injectable()
 
 export class TaskService {
-  public getTasks(): Array<Task> {
-    return TASKS;
+  public getTasks(): Promise<Task[]> {
+    let promise = new Promise((resolve, reject) => {
+      if (TASKS.length > 0){
+        resolve(TASKS);
+      }else{
+        let error_msg = 'Not tasks!';
+        reject(error_msg);
+      }
+    });
+
+    return promise;
   }
 }
