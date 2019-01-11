038c3b496882fe2e28b1f8741459ef29d882c32f
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2019-01-10 21:47:54 -0300

    initial implementation of task service

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,31 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Task } from './shared/task.model';
-
-const TASKS: Array<Task> = [
-  { id: 1, title: 'Fazer Tarefa 1' },
-  { id: 2, title: 'Fazer Tarefa 2' },
-  { id: 3, title: 'Fazer Tarefa 3' },
-  { id: 4, title: 'Fazer Tarefa 4' },
-  { id: 5, title: 'Fazer Tarefa 5' },
-  { id: 6, title: 'Fazer Tarefa 6' },
-  { id: 7, title: 'Fazer Tarefa 7' }
-];
+import { TaskService } from './shared/task.service';
 
 @Component({
   selector: 'tasks',
-  templateUrl: './tasks.component.html'
+  templateUrl: './tasks.component.html',
+  providers: [TaskService]
 })
 
 export class TasksComponent implements OnInit {
-  public tasks;
+  public tasks: Array<Task>;
   public selectedTask: Task;
+  private taskService: TaskService;
 
-  public constructor() {
+  public constructor(taskService: TaskService) {
+    this.taskService = taskService;
   }
 
   public ngOnInit() {
-    this.tasks = TASKS;
+    this.tasks = this.taskService.getTasks();
   }
 
   public onSelect(task: Task): void {
