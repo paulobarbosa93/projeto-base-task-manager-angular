6c0bd1c99ac9dc06de7aa43fd0385bacde2f57e6
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2019-01-15 23:28:17 -0300

    learning observables

--- src/app/learning-observables.service.ts
+++ src/app/learning-observables.service.ts
@@ -1,19 +1,24 @@
-import { Http } from '@angular/http'
-import { Injectable } from '@angular/core'
+import { Http, Response } from '@angular/http';
+import { Injectable } from '@angular/core';
+
+// errors
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/Observable/throw';
 
 @Injectable()
 
 export class LearningObservables {
-  public constructor(private http: Http){
+  public constructor(private http: Http) {
     // creating observer object
     let observer = {
-      next: function(newData){
+      next: function(newData) {
         console.log("Chamou o método next e passou como parâmetro o 'newData' => ", newData);
       },
-      error: function(errorData){
+      error: function(errorData) {
         console.log("Chamou o método error e passou como parâmetro o 'errorData' => ", errorData);
       },
-      complete: function(){
+      complete: function() {
         console.log("Chamou o método complete e encerrou");
       }
     };
@@ -60,11 +65,30 @@ export class LearningObservables {
     //     }
     //   );
 
-    this.http.get('api/tasks')
-      .subscribe(
-        newData => console.log("Chamou o método next e passou como parâmetro o 'newData' => ", newData),
-        errorData => console.log("Chamou o método error e passou como parâmetro o 'errorData' => ", errorData),
-        () => console.log("Chamou o método complete e encerrou")
-      );
+    // this.http.get('api/tasks')
+    //   .subscribe(
+    //     newData => console.log("Chamou o método next e passou como parâmetro o 'newData' => ", newData),
+    //     errorData => console.log("Chamou o método error e passou como parâmetro o 'errorData' => ", errorData),
+    //     () => console.log("Chamou o método complete e encerrou")
+    //   );
+
+    this.http.get('api/taskss')
+      .catch(this.handlerErrors)
+      .subscribe({
+        next: function(newData) {
+          console.log("Chamou o método next e passou como parâmetro o 'newData' => ", newData);
+        },
+        error: function(errorData) {
+          console.log("Chamou o método error e passou como parâmetro o 'errorData' => ", errorData);
+        },
+        complete: function() {
+          console.log("Chamou o método complete e encerrou");
+        }
+      });
+  }
+
+  public handlerErrors(error: Response) {
+    console.log("salvando erro em banco de dados para o desenvolvedor => ", error);
+    return Observable.throw(error);
   }
 }
