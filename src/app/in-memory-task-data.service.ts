6b00b650e0a016dfce24037b2caf61f9ab17fb2e
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2019-01-18 16:57:47 -0300

    adding task search component base

--- src/app/in-memory-task-data.service.ts
+++ src/app/in-memory-task-data.service.ts
@@ -6,13 +6,13 @@ import { InMemoryDbService } from 'angular-in-memory-web-api'
 export class InMemoryTaskDataService implements InMemoryDbService{
   public createDb(){
     let tasks = [
-      { id: 1, title: 'Fazer Tarefa 1' },
-      { id: 2, title: 'Fazer Tarefa 2' },
-      { id: 3, title: 'Fazer Tarefa 3' },
-      { id: 4, title: 'Fazer Tarefa 4' },
-      { id: 5, title: 'Fazer Tarefa 5' },
-      { id: 6, title: 'Fazer Tarefa 6' },
-      { id: 7, title: 'Fazer Tarefa 7' }
+      { id: 1, title: 'Ir a academia' },
+      { id: 2, title: 'Pagar boleto' },
+      { id: 3, title: 'Seguir uma dieta' },
+      { id: 4, title: 'Ler 24 livros' },
+      { id: 5, title: 'Estudar mais' },
+      { id: 6, title: 'Abrir um negócio próprio' },
+      { id: 7, title: 'Comprar um carro' }
     ];
 
     return { tasks }
