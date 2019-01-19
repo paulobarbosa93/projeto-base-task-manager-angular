17951cfe1376d05f66182e84319df931dc0ec0a5
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2019-01-18 22:27:52 -0300

    adding the search component logic

--- src/app/navbar/task-search/task-search.component.ts
+++ src/app/navbar/task-search/task-search.component.ts
@@ -1,17 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Task } from '../../tasks/shared/task.model';
 import { TaskService } from '../../tasks/shared/task.service';
+
+import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/Observable/of';
 
 @Component({
   selector: 'task-search',
   templateUrl: './task-search.component.html'
 })
 
-export class TaskSearchComponent {
-  public constructor(private taskService: TaskService) { }
+export class TaskSearchComponent implements OnInit {
+  public searchTerms: Subject<string> = new Subject();
+  public tasks: Task[] = [];
 
-  public search(term: string): void {
-    console.log(term);
+  public constructor(private taskService: TaskService, private router: Router) { }
+
+  public ngOnInit() {
+    this.searchTerms.switchMap(
+      term => term ? this.taskService.searchByTitle(term) : Observable.of<Task[]>([])
+    ).subscribe(tasks => this.tasks = tasks);
+  }
+
+  public search(term: string) {
+    this.searchTerms.next(term);
+  }
+
+  public goToTask(task: Task) {
+    this.tasks = [];
+    this.router.navigate(['/tasks', task.id]);
   }
 }
