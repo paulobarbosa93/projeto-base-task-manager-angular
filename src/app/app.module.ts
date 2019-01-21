2f90801d8246ef8bdcc212eae62298698d35cb5e
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2019-01-21 11:09:00 -0300

    adding datetimepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,7 @@ import 'rxjs/add/Observable/throw';
 
 // jquery plugins
 import * as $ from 'jquery';
+import * as datetimepicker from 'eonasdan-bootstrap-datetimepicker';
 
 @NgModule({
   declarations: [
