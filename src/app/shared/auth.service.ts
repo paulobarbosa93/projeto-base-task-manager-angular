eeea28e25fb78feee354bb3ee02f44edd88c5c2f
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2019-01-25 21:53:21 -0300

    improving signup

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -11,9 +11,9 @@ import { User } from './user.model';
 export class AuthService {
   public constructor(private tokenService: Angular2TokenService) { }
 
-  public signUp(user: User) {
-    // call Angular2-Token signUp method
-    // returns a Observable<Response>
+  public signUp(user: User) : Observable<Response> {
+    return this.tokenService.registerAccount(user as any)
+      .catch(this.handlerErrors);
   }
 
   public signIn(uid: string, password: string) {
--- src/app/sign-up-form/sign-up-form.component.ts
+++ src/app/sign-up-form/sign-up-form.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router'
 
+import { AuthService } from '../shared/auth.service'
 import { FormUtils } from '../shared/form.utils'
+import { User } from '../shared/user.model'
 
 @Component({
   selector: 'sign-up-form',
@@ -13,21 +16,22 @@ export class SignUpFormComponent {
   public formUtils: FormUtils;
 
   public constructor(
-    private formBuilder: FormBuilder
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private router: Router,
   ) {
-    this.form = this.formBuilder.group({
-      name: [null, [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
-      email: [null, [Validators.required, Validators.email]],
-      password: [null, [Validators.required, Validators.minLength(8)]],
-      passwordConfirmation: [null, [Validators.required]]
-    }, { validator: this.passwordConfirmationValidator });
-
+    this.setupForm();
     this.formUtils = new FormUtils(this.form);
   }
 
   public signUpUser() {
-    console.log('Enviado!');
-    console.log(this.form.value);
+    this.authService.signUp(this.form.value as User)
+      .subscribe(
+        () => {
+          alert('Conta criada com sucesso!');
+          this.router.navigate(['/dashboard']);
+        }
+      );
   }
 
   public passwordConfirmationValidator(form: FormGroup){
@@ -35,5 +39,14 @@ export class SignUpFormComponent {
       form.get('passwordConfirmation').setErrors(null);
     else
       form.get('passwordConfirmation').setErrors({'mismatch': true});
+  }
+
+  public setupForm() {
+    this.form = this.formBuilder.group({
+      name: [null, [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, Validators.minLength(8)]],
+      passwordConfirmation: [null, [Validators.required]]
+    }, { validator: this.passwordConfirmationValidator });
   }
 }
